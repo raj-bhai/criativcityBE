e7f55278768776f5c05aa17e7f31dde14af05863
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-10 23:28:01 +0530

    decrypt

--- controllers/razorpay.js
+++ controllers/razorpay.js
@@ -1,4 +1,3 @@
-const Razorpay = require('razorpay');
 require('dotenv').config();
 const crypto = require('crypto');
 const axios = require('axios');
@@ -18,167 +17,6 @@ function generateBdTimestamp() {
   return timestamp;
 }
 
-function generateInvoiceDate() {
-  // Get the current date and time
-  const now = new Date();
-
-  // Customize the date as needed
-  // Example: Setting the invoice date to 3 days from the current date
-  now.setDate(now.getDate() + 3);
-
-  // Format the date as per the desired format
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-
-  const invoiceDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}+05:30`;
-  return invoiceDate;
-}
-
-// exports.createOrder = async (req, res, next) => {
-//   try {
-
-//     // ---------------------------------------------------------------------Razorpay------------------------------------------------
-
-//     // const razorpay = new Razorpay({
-//     //     key_id: process.env.RAZORPAY_KEY_ID,
-//     //     key_secret: process.env.RAZORPAY_KEY_SECRET,
-//     // });
-
-//     // const amount = req.body.amount; // Amount in paise (i.e., 1000 paise = INR 10)
-//     // const currency = 'INR';
-//     // const options = {
-//     //     amount: amount,
-//     //     currency: currency,
-//     //     receipt: 'order_rcptid_11',
-//     //     payment_capture: 1,
-//     // };
-
-//     // razorpay.orders.create(options, function (err, order) {
-//     //     if (err) {
-//     //         console.log(err);
-//     //         res.status(400).json({
-//     //             message: err,
-//     //             success: false
-//     //         });
-//     //     } else {
-//     //         console.log("order :",order);
-//     //         res.status(200).json({
-//     //             success: true,
-//     //             data: order
-//     //         })
-//     //     }
-//     // });
-
-
-
-//     // -----------------------------------------------------------Stripe----------------------------------------------------------
-
-//     // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-//     // const amount  = req.body.amount;
-
-//     // try {
-//     //     const paymentIntent = await stripe.paymentIntents.create({
-//     //         currency: 'INR',
-//     //         metadata: { integration_check: 'accept_a_payment' },
-//     //         amount : amount
-//     //     });
-//     //     res.status(200).json({
-//     //         clientSecret: paymentIntent.client_secret
-//     //     })
-//     // } catch (error) {
-//     //     console.error(error);
-//     //     res.status(500).json({ error: error });
-//     // }
-
-//     // --------------------------------------------------------------BillDesk---------------------------------------- 
-
-//     const apiUrl = 'https://pguat.billdesk.io/payments/ve1_2/orders/create';
-//     const sharedSecretKey = process.env.BILLDESK_SECRET_KEY;
-//     const clientId = process.env.BILLDESK_CLIENTID; 
-
-//     const jwsHeader = {
-//       alg: 'HS256',
-//       clientid: clientId
-//     };
-//     const encodedHeader = base64UrlEncode(JSON.stringify(jwsHeader));
-
-//     const payload = {
-//       "mercid": process.env.BILLDESK_MERCHANTID,
-//       "orderid": 'order45608988',
-//       "amount": '300.00',
-//       "order_date": '2021-03-05T10:59:15+05:30',
-//       "currency": '356',
-//       "ru" : 'https://merchant.com',
-//       "additional_info": {
-//         "additional_info1": 'Details1',
-//         "additional_info2": 'Details2'
-//         },
-//         "itemcode": 'DIRECT',
-//         "device": {
-//         "init_channel" : 'internet',
-//         "ip": '124.124.1.1',
-//         "mac": '11-AC-58-21-1B-AA',
-//         "imei": '990000112233445',
-//         "accept_header": 'text/html',
-//         "fingerprintid": '61b12c18b5d0cf901be34a23ca64bb19'
-//         },
-//     };
-//     const encodedPayload = base64UrlEncode(JSON.stringify(payload));
-
-
-//     const signature = crypto.createHmac('sha256', sharedSecretKey)
-//       .update(`${encodedHeader}.${encodedPayload}`)
-//       .digest('base64');
-//     const encodedSignature = base64UrlEncode(signature);
-
-//     const jwsHmacToken = `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
-
-//     const headers = {
-//       'Content-Type': 'application/jose',
-//       'bd-timestamp': generateBdTimestamp(),
-//       'accept': 'application/jose',
-//       'bd-traceid': '20200817132207ABD1K',
-//       'Authorization': `Bearer ${jwsHmacToken}`
-//     };
-
-//     console.log("payload :", payload)
-//     console.log("Headers :", headers)
-
-//     axios.post(apiUrl, payload, { headers })
-//       .then(response => {
-//         // Process the API response
-//         if (response.status === 200) {
-//           const data = response.data;
-//           // Process the returned data
-//           console.log(data);
-//           res.status(200).json(data)
-//         } else {
-//           throw new Error(`API request failed with status code: ${response.status}`);
-//         }
-//       })
-//       .catch(error => {
-//         console.error('Error:', error.message);
-//       });
-
-
-//   } catch (err) {
-//     console.log("err :", err);
-//     res.status(400).json({
-//       message: err,
-//       success: false
-//     });
-//   }
-// }
-
-
-// Function to encode a string using base64url encoding
-
-
 
 function base64UrlEncode(input) {
   let base64 = Buffer.from(input).toString('base64');
@@ -186,11 +24,18 @@ function base64UrlEncode(input) {
   return base64;
 }
 
+const decryptData = (encryptedData, secretKey) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
+  const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+  return decryptedData;
+};
+
+
 
 exports.createOrder = async (req, res, next) => {
   try {
 
-    const {orderId, amount} = req.body;
+    const { orderId, amount } = req.body;
     const currentDate = new Date();
     const formattedDate = currentDate.toISOString().replace(/\.\d{3}/, '') + '+0530';
 
@@ -260,13 +105,12 @@ exports.createOrder = async (req, res, next) => {
       'Accept': 'application/jose',
       'bd-traceid': `${timestamp}ABD1K`
     };
-    //     console.log(" Trace Id :",`${timestamp}ABD1K` )
-    //     console.log(" order Date :", formattedDate)
-    //     console.log("TimeStamp :", timestamp);
-    // console.log("JwsToken :", jwsToken)
     const response = await axios.post('https://pguat.billdesk.io/payments/ve1_2/orders/create', jwsToken, { headers });
 
-    console.log('Response:', response.data);
+    // console.log('Response:', response.data);
+
+    const decryptedData = decryptData(response.data, secretKey);
+    console.log('Decrypted Data:', decryptedData);
     res.status(200).json({ data: response.data });
   } catch (error) {
     // console.log("Error:", error);
